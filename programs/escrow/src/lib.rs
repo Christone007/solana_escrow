1f9428605410704f7ad604432cebd1d2d410d6f0
Christone007 <exellentemy@gmail.com>
2026-09-10 09:19:03 +0100

    Add take and update instructions to entry point

--- programs/escrow/src/lib.rs
+++ programs/escrow/src/lib.rs
@@ -19,4 +19,12 @@ use super::*;
     pub fn make(ctx: Context<Make>, amount_a: u64, amount_b: u64, seed: u64) -> Result<()> {
         make::handle_make(ctx, amount_a, amount_b, seed)
     }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        take::handle_take(ctx)
+    }
+
+    pub fn update(ctx: Context<Update>, new_amount: u64) -> Result<()> {
+        update::handle_update(ctx, new_amount)
+    }
 }
